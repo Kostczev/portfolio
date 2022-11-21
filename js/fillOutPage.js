143db7afbaf7d90cc50e5b9ae8b021650c2e6fe2
Kostczev <churczev.k@gmail.com>
2022-11-21 19:53:27 +0500

    add work data json dell data.js creat data.json

--- js/fillOutPage.js
+++ js/fillOutPage.js
@@ -1,57 +1,36 @@
 const worksContainer = document.querySelector('.works');
 
+fetch('./js/data.json')
+   .then(resp => resp.json())
+   .then(json => appendWorks(json));
 
-for(let work in worksArrea) {
-   let block = document.createElement('div');
-   block.className = "work item";
+function appendWorks(data){
+   data.forEach(element => {
+      let block = document.createElement('div');
+      block.className = "work item";
 
-   let title = document.createElement('div');
-   title.className = "work__title";
-   let titleText = '';
-   if(worksArrea[work]['titelLinkText'] != '') {
-      titleText += `<a class="link" href="${worksArrea[work]['titleLink']}" target="_blank">${worksArrea[work]['titelLinkText']}</a> : `;
-   }
-   titleText += worksArrea[work]['title'];
-   title.innerHTML = titleText;
+      let title = document.createElement('div');
+      title.className = "work__title";
+      let titleText = '';
+      if(element.titelLinkText != '') {
+         titleText += `<a class="link" href="${element.titleLink}" target="_blank">${element.titelLinkText}</a> : `;
+      }
+      titleText += element.title;
+      title.innerHTML = titleText;
 
-   let image = document.createElement('div');
-   image.className = "work__image";
-   image.innerHTML = `<img class="item" src="${worksArrea[work]['linkImage']}" alt="">`
+      let image = document.createElement('div');
+      image.className = "work__image";
+      image.innerHTML = `<img class="item" src="${element.linkImage}" alt="">`
 
-   let links = document.createElement('div');
-   links.className = "work__links";
-   links.innerHTML = `<a href="${worksArrea[work]['linkGit']}" target="_blank" class="work__link link">git</a>
-   <a href="${worksArrea[work]['linkBeb']}" target="_blank" class="work__link link">На сайт</a>`;
-   
-   block.append(title);
-   block.append(image);
-   block.append(links);
+      let links = document.createElement('div');
+      links.className = "work__links";
+      links.innerHTML = `<a href="${element.linkGit}" target="_blank" class="work__link link">git</a>
+      <a href="${element.linkBeb}" target="_blank" class="work__link link">На сайт</a>`;
+      
+      block.append(title);
+      block.append(image);
+      block.append(links);
 
-   worksContainer.prepend(block);
+      worksContainer.append(block);
+   });
 }
-
-//2й способ вставки в документ
-//чтоб не заполнять новые работы снизу списка в date переворачиваем выполнение с помощью доп строки
-//let works = '';
-//for(let work in worksArrea) {
-//   let block = '';
-//   block += '<div class="work item">';
-//   block += '<div class="work__title">';
-//   if(worksArrea[work]['titelLinkText'] != '') {
-//      console.log(worksArrea[work]['titleLink']);
-//      console.log(worksArrea[work]['titleLinkText']);
-//      block += `<a class="link" href="${worksArrea[work]['titleLink']}" target="_blank">${worksArrea[work]['titelLinkText']}</a> : `;
-//   }
-//   block += `${worksArrea[work]['title']}</div>`;
-//   block += '<div class="work__image">';
-//   block += `<img class="item" src="${worksArrea[work]['linkImage']}" alt="">`;
-//   block += '</div>';
-//   block += '<div class="work__links">';
-//   block += `<a href="${worksArrea[work]['linkGit']}" target="_blank" class="work__link link">git</a>`;
-//   block += `<a href="${worksArrea[work]['linkBeb']}" target="_blank" class="work__link link">На сайт</a>`;
-//   block += '</div>';
-//   block += '</div>';
-//   block += works;
-//   works = block;
-//}
-//worksContainer.innerHTML = works;
